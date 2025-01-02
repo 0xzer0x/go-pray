ef4a8ec97089d2f4363e31ff423d662eae05750e
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-01-02 10:04:22 +0200

    fix: check if notify-send exists before using

--- internal/util/notifications.go
+++ internal/util/notifications.go
@@ -1,13 +1,22 @@
 package util
 
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func SendNotification(icon, body string) error {
+	var err error
+	var notifyCmd string
+	if notifyCmd, err = exec.LookPath("notify-send"); err != nil {
+		return fmt.Errorf("command '%s' does not exist", "notify-send")
+	}
+
 	var cmd *exec.Cmd
 	if len(icon) > 0 {
-		cmd = exec.Command("notify-send", "--icon="+icon, "go-pray", body)
+		cmd = exec.Command(notifyCmd, "--icon="+icon, "go-pray", body)
 	} else {
-		cmd = exec.Command("notify-send", "go-pray", body)
+		cmd = exec.Command(notifyCmd, "go-pray", body)
 	}
 	return cmd.Run()
 }
