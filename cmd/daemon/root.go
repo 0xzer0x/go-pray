24c7cbe66e44f9d8ab27104f4058f961286c6f69
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-01-06 20:15:21 +0200

    feat: use work groups to create goroutines for prayer timers

--- cmd/daemon/root.go
+++ cmd/daemon/root.go
@@ -1,8 +1,8 @@
 package daemon
 
 import (
-	"fmt"
-	"strings"
+	"log"
+	"sync"
 	"time"
 
 	"github.com/mnadev/adhango/pkg/calc"
@@ -30,54 +30,77 @@ var DaemonCmd = &cobra.Command{
 	Run: daemonCmd,
 }
 
+func notifyPrayer(
+	player *adhan.Player,
+	calendar *calc.PrayerTimes,
+	prayer calc.Prayer,
+	fromNow time.Duration,
+) {
+	timer := time.NewTimer(fromNow)
+	name := common.CalendarName(*calendar, prayer)
+	notifyChan := make(chan notify.Result)
+
+	<-timer.C
+	log.Printf("notification timer finished: %s\n", name)
+	go notify.SendInteractive(
+		notifyChan,
+		"clock-applet-symbolic",
+		"Time for "+name+" prayer 🕌",
+		player.Duration(),
+	)
+	if err := player.Play(); err != nil {
+		util.ErrExit("%v", err)
+	}
+
+	result := <-notifyChan
+	if result.Error != nil {
+		util.ErrExit("failed to send notification: %v", result.Error)
+	}
+	if result.Clicked {
+		player.Stop()
+	}
+}
+
 func daemonCmd(cmd *cobra.Command, ars []string) {
-	fmt.Println("starting in daemon mode")
+	log.Println("starting in daemon mode")
 
 	player := adhan.NewPlayer()
 	if err := player.Initialize(); err != nil {
 		util.ErrExit("%v", err)
 	}
 
-	prayerTimes, err := ptime.CurrentPrayerTimes()
-	if err != nil {
-		util.ErrExit("%v", err)
-	}
-
+	var err error
+	var prayerTimes calc.PrayerTimes
 	for {
-		if nxt := prayerTimes.NextPrayerNow(); nxt == calc.NO_PRAYER {
-			prayerTimes, err = ptime.DatePrayerTimes(time.Now().UTC().AddDate(0, 0, 1))
-			if err != nil {
-				util.ErrExit("%v", err)
-			}
-		}
-
-		nextPrayer := prayerTimes.NextPrayerNow()
-		nextName := common.CalendarName(*prayerTimes, nextPrayer)
-
-		fmt.Printf("next prayer: %s\n", strings.ToLower(nextName))
-		timeRemaining := time.Until(prayerTimes.TimeForPrayer(nextPrayer))
-		fmt.Printf("time remaining: %s\n", timeRemaining.String())
-		notifyTimer := time.NewTimer(timeRemaining)
-		notifyChan := make(chan notify.Result)
-
-		<-notifyTimer.C
-		fmt.Println("sending prayer notification")
-		go notify.SendInteractive(
-			notifyChan,
-			"clock-applet-symbolic",
-			"Time for "+nextName+" prayer 🕌",
-			player.Duration(),
-		)
-		if err := player.Play(); err != nil {
+		// INFO: get prayers calendar which the upcoming prayer belongs to
+		prayerTimes, _, err = ptime.NextPrayer()
+		if err != nil {
 			util.ErrExit("%v", err)
 		}
 
-		result := <-notifyChan
-		if result.Error != nil {
-			util.ErrExit("failed to send notification: %v", result.Error)
+		// INFO: create a WaitGroup for the prayers in the calendar with a future date
+		var wg sync.WaitGroup
+		for name, prayer := range common.Prayers {
+			if prayer == calc.SUNRISE {
+				continue
+			}
+
+			remainingForPrayer := time.Until(prayerTimes.TimeForPrayer(prayer))
+			if remainingForPrayer > 0 {
+				log.Printf(
+					"creating prayer timer: %s - remaining: %s\n",
+					name,
+					remainingForPrayer.Truncate(time.Second).String(),
+				)
+
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					notifyPrayer(player, &prayerTimes, prayer, remainingForPrayer)
+				}()
+			}
 		}
-		if result.Clicked {
-			player.Stop()
-		}
+
+		wg.Wait()
 	}
 }
