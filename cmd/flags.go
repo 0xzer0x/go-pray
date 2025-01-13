956404cc76f6b457582f10b649100082348f51bb
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-01-13 08:57:27 +0200

    refactor: separate root cmd into two files

---
+++ cmd/flags.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"github.com/spf13/viper"
+
+	"github.com/0xzer0x/go-pray/internal/util"
+)
+
+func registerGlobalFlags() {
+	// NOTE: register string flags
+	for name, usage := range map[string]string{
+		"config":   "config file",
+		"timezone": "calculation timezone",
+		"method":   "calculation method",
+		"format":   "output format",
+		"adhan":    "path to adhan mp3",
+	} {
+		rootCmd.PersistentFlags().String(name, "", usage)
+		err := viper.BindPFlag(name, rootCmd.PersistentFlags().Lookup(name))
+		if err != nil {
+			util.ErrExit("failed to bind %s flag", name)
+		}
+	}
+
+	// NOTE: register float flags
+	for name, usage := range map[string]string{
+		"location.lat":  "calculation latitude",
+		"location.long": "calculation longitude",
+	} {
+		rootCmd.PersistentFlags().Float64(name, 0, usage)
+		err := viper.BindPFlag(name, rootCmd.PersistentFlags().Lookup(name))
+		if err != nil {
+			util.ErrExit("failed to bind %s flag", name)
+		}
+	}
+}
--- cmd/root.go
+++ cmd/root.go
@@ -4,49 +4,18 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 
 	"github.com/0xzer0x/go-pray/cmd/calendar"
 	"github.com/0xzer0x/go-pray/cmd/daemon"
 	"github.com/0xzer0x/go-pray/cmd/next"
 	"github.com/0xzer0x/go-pray/cmd/version"
 	"github.com/0xzer0x/go-pray/internal/config"
-	"github.com/0xzer0x/go-pray/internal/util"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "go-pray",
 	Short: "Prayer times CLI",
 	Long:  `Prayer times CLI`,
-}
-
-func registerGlobalFlags() {
-	// NOTE: register string flags
-	for name, usage := range map[string]string{
-		"config":   "config file",
-		"timezone": "calculation timezone",
-		"method":   "calculation method",
-		"format":   "output format",
-		"adhan":    "path to adhan mp3 to use",
-	} {
-		rootCmd.PersistentFlags().String(name, "", usage)
-		err := viper.BindPFlag(name, rootCmd.PersistentFlags().Lookup(name))
-		if err != nil {
-			util.ErrExit("failed to bind %s flag", name)
-		}
-	}
-
-	// NOTE: register float flags
-	for name, usage := range map[string]string{
-		"location.lat":  "calculation latitude",
-		"location.long": "calculation longitude",
-	} {
-		rootCmd.PersistentFlags().Float64(name, 0, usage)
-		err := viper.BindPFlag(name, rootCmd.PersistentFlags().Lookup(name))
-		if err != nil {
-			util.ErrExit("failed to bind %s flag", name)
-		}
-	}
 }
 
 func init() {
